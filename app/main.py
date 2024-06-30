a6ff55c0cf09fae3a57f384ea35458c7c05eb205
randomrantsbro <randomrantsbro@gmail.com>
2024-06-30 16:30:33 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ app.include_router(gemini.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
